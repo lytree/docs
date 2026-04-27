b94f2d76ceb920bd18ae416c4b2404dde417b654
yangzh <yangzh@windit.com.cn>
2026-04-27 09:51:16 +0800

    add DB

--- app/lib/layout.shared.tsx
+++ app/lib/layout.shared.tsx
@@ -1,5 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import { appName } from './shared';
 
 export function baseOptions(): BaseLayoutProps {
   return {
